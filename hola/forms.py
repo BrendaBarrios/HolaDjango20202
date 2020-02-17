6e247b4bbc9dae82f23d8237d6409ea12d245de0
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2020-02-16 19:24:29 -0600

    Agregando validadores para el campo nombre

--- hola/forms.py
+++ hola/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import Articulo
+from .models import Articulo, LONGITUD_MAXIMA
+
+
 
 class FormArticulo(forms.ModelForm):
 
@@ -7,3 +9,8 @@ class FormArticulo(forms.ModelForm):
         model = Articulo
         fields = '__all__'
         # fields = ('nombre', 'precio')
+
+        error_messages = {
+            'nombre': {'max_length':LONGITUD_MAXIMA ,
+            'required': 'Este campo es requerido'}
+        }
--- hola/models.py
+++ hola/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
+
+LONGITUD_MAXIMA='Error de longitud'
 
 class Articulo(models.Model):
-    nombre = models.CharField('Nombre',max_length=100)
+    nombre = models.CharField('Nombre',max_length=100,validators=[MaxLengthValidator(100,LONGITUD_MAXIMA)])
     precio = models.DecimalField('Precio Unitario', max_digits=5, decimal_places=2)
     descripcion = models.TextField('Descripción', max_length=300)
     imagen = models.ImageField('Imagen', upload_to=None, null=True, blank=True)
